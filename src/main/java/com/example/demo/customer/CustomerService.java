3ccd0f09a75f94e1b5c9450402f8f0345334e2d7
minatisleeping <minatt2002@gmail.com>
2024-01-09 15:51:40 +0700

    Spring Boot Actuator and Metrics

--- src/main/java/com/example/demo/customer/CustomerService.java
+++ src/main/java/com/example/demo/customer/CustomerService.java
@@ -1,17 +1,17 @@
 package com.example.demo.customer;
 
 import com.example.demo.exception.NotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
 @Service
 public class CustomerService {
 
+    private final static Logger LOGGER = LoggerFactory.getLogger(CustomerService.class);
     private final CustomerRepository customerRepository;
 
     @Autowired
@@ -20,6 +20,7 @@ public class CustomerService {
     }
 
     List<Customer> getCustomers() {
+        LOGGER.info("getCustomers was called");
         return customerRepository.findAll();
     }
 
@@ -27,7 +28,11 @@ public class CustomerService {
         return customerRepository
                 .findById(id)
                 .orElseThrow(
-                        () -> new NotFoundException(
-                                "customer with id " + id + " not found!"));
+                        () -> {
+                            NotFoundException notFoundException = new NotFoundException(
+                                    "customer with id " + id + " not found");
+                            LOGGER.error("error in getting customer {}", id, notFoundException);
+                            return notFoundException;
+                        });
     }
 }
--- src/main/java/com/example/demo/exception/ApiExceptionHandler.java
+++ src/main/java/com/example/demo/exception/ApiExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.example.demo.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -9,6 +11,8 @@ import java.time.ZonedDateTime;
 
 @ControllerAdvice
 public class ApiExceptionHandler {
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(ApiExceptionHandler.class);
 
     @ExceptionHandler(value = ApiRequestExcepton.class)
     public ResponseEntity<Object> handleApiRequestException(ApiRequestExcepton e)  {
